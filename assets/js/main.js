aa2ed2b36f9f19d8d0692790246af66391200bd0
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-24 15:59:11 +0100

    Programma JS che stampa in console il risultato

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,15 @@ Per sapere se un numero è divisibile per un altro utilizzo la % per estrarre il
 • BONUS 2: 
     Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 */
+
+for(let n=1; n <= 100; n++){
+    if( n % 3 == 0 && n % 5 == 0){
+        console.log("FizzBuzz")
+    } else if(n % 3 == 0){
+        console.log("Fizz")
+    } else if(n % 5 == 0){
+        console.log("Buzz")
+    } else{
+        console.log(n)
+    }
+}
